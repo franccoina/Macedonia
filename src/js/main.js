830ea9ab7e4111d9f8c02f85c98b65b8fb1aabf5
David Francisco Blandón Mena <franccoina@gmail.com>
2024-05-25 00:19:45 -0500

    feat:home and contacta files, css, js and html

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+let nav = document.getElementById("navbarBtn");
+nav.addEventListener("click", () => {
+  console.log("entrando");
+  document.body.classList.toggle("open");
+});
