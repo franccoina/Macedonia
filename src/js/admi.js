08150ca88e309d26722b25a8f4b7c2714b038f03
David Francisco Blandón Mena <franccoina@gmail.com>
2024-05-26 17:23:42 -0500

    fix:change of admi.js

--- src/js/admi.js
+++ src/js/admi.js
@@ -12,10 +12,12 @@ nav.addEventListener("click", () => {
   document.body.classList.toggle("open");
 });
 
+//funcion para el login
+
 (function () {
     let userOnline = localStorage.getItem("userOnline");
     if (userOnline != null) {
-        window.location.href = "./src/pages/dashboard.html";
+        window.location.href = "../pages/create.html";
     }
 })();
 
@@ -25,33 +27,34 @@ let form = document.querySelector("form");
 
 //llamar a los campos del formulario
 
-let username = document.querySelector("#user");
+let username = document.querySelector("#username");
 let password = document.querySelector("#password");
 
 form.addEventListener("submit", async function (event) {
     event.preventDefault();
-    let user = await checkEmail(email);
+    let user = await checkUsername(username);
     if (user === false) {
-        alert("El email no existe");
-    } else {
+        alert("El usuario no existe");
+    } 
+    else {
         if (user.password === password.value) {
             alert("Bienvenido");
             localStorage.setItem("userOnline", JSON.stringify(user));
-            window.location.href = "./src/pages/dashboard.html";
+            window.location.href = "../pages/create.html";
         } else {
             alert("La contraseña no es correcta");
         }
     }
 });
 
-async function checkEmail(email) {
-    //traemos a todos los usuarios que tengan el email que se ingreso
+async function checkUsername(username) {
+    //traemos a todos los usuarios que tengan el username que se ingreso
     let response = await fetch(
-        `http://localhost:3000/users?email=${email.value}`
+        `http://localhost:3000/users?username=${username.value}`
     );
     let datos = await response.json();
 
-    //verificamos que el email no este regitrado
+    //verificamos que el username no este registrado
     if (datos.length === 1) {
         return datos[0];
     } else {
